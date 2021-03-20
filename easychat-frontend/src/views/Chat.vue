c29af6f028886dedd404281fba5c5d4f3cfadf2a
DannyZeng2 <15625140174@163.com>
2021-03-20 12:39:19 +0800

    样式兼容不同分辨率

--- easychat-frontend/src/views/Chat.vue
+++ easychat-frontend/src/views/Chat.vue
@@ -9,8 +9,7 @@
 <template>
   <div id="main">
 
-    <div id="main-content">
-      <el-container>
+      <el-container id="main-content">
         <el-aside style="width: 80px;">
           <el-menu :default-active="activeIndex" class="left-menu"  @select="handleSelect" background-color="#1d3e6d">
             <div id="photo"><el-avatar :size="60" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" style="margin: 10px"></el-avatar></div>
@@ -19,34 +18,44 @@
             <el-menu-item index="3"> <i class="el-icon-s-tools" style="width: 40px"></i></el-menu-item>
           </el-menu>
         </el-aside>
+
         <el-aside>
-          <el-menu :default-active="activeIndex" class="main-menu" background-color="#e8e8e8">
+          <el-menu id="user-list" :default-active="1" class="main-menu" background-color="#e8e8e8" >
             <div id="user-list-title"></div>
             <el-divider></el-divider>
-            <el-menu-item index="1"></el-menu-item>
-            <el-menu-item index="2"></el-menu-item>
-            <el-menu-item index="3"></el-menu-item>
+            <el-menu-item index="1">
+              <el-avatar shape="square" :size="50" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+              <span class="chat-member">Danny Zeng</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <el-avatar shape="square" :size="50" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+              <span class="chat-member">Eric Chen</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <el-avatar shape="square" :size="50" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+              <span class="chat-member">Matthew Li</span>
+            </el-menu-item>
           </el-menu>
         </el-aside>
+
         <el-container>
           <el-header id="chat-title" height="60px"></el-header>
           <el-divider></el-divider>
           <el-main id="chat-content"><div id="content"></div></el-main>
-          <el-footer id="tool-field" height="50px">
+          <el-footer id="tool-field" height="5%">
             <i class="el-icon-picture-outline" ></i>
             <i class="el-icon-video-camera"></i>
             <i class="el-icon-folder-add"></i>
           </el-footer>
-          <el-footer id="input-field" height="360px">
+          <el-footer id="input-field" height="20%">
             <textarea id='input' style="width: 95%;height: 100% " v-model="input"></textarea>
           </el-footer>
-          <el-footer id="send-msg" height="50px"><el-button id="send-msg-btn" type="primary" @click="sendMsg" style="width: 8%">Send</el-button></el-footer>
+          <el-footer id="send-msg" height="50px"><el-button id="send-msg-btn" type="primary" @click="sendMsg" style="width: 12%">Send</el-button></el-footer>
           <div ></div>
           <div></div>
         </el-container>
       </el-container>
     </div>
-  </div>
 </template>
 <script>
 const ENTER = 0
@@ -58,7 +67,8 @@ export default {
     return {
       input: '',
       content: '',
-      message: ''
+      message: '',
+      activeIndex:1
     }
   },
 
@@ -125,19 +135,19 @@ export default {
 #main-content {
   background: white;
   width: 60%;
-  height: 1200px;
+  height: 80%;
   margin: auto;
-  margin-top: 90px;
+  margin-top: 5%;
+
 }
 
 .left-menu {
-  height:1200px;
+  height:100%;
 }
 
 .main-menu{
-  height:1200px;
+  height:100%;
 }
-
 
 #user-list-title{
   height: 60px;
@@ -155,8 +165,8 @@ export default {
   font-size:200%
 }
 .el-icon-picture-outline,.el-icon-video-camera,.el-icon-folder-add{
-  font-size:220%;
-  margin: 10px;
+  font-size:200%;
+  margin: 5px;
 }
 
 #send-msg{
@@ -177,9 +187,23 @@ textarea{
   resize: none;
   cursor: pointer;
   outline:none;
-  font-size:30px
+  font-size:20px
 }
 
+.el-menu-item {
+  font-size: 14px;
+  padding: 0 10px;
+  cursor: pointer;
+  height: 60px;
+  box-sizing: border-box;
+}
 
+.chat-member{
+  margin-left: 20px;
+  font-size: 18px;
+}
+#user-list .el-menu-item.is-active{
+  background-color:#bcbcbc !important;
+}
 
 </style>
